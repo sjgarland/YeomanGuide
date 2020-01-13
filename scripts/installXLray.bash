ff9285d08b77e9ade754d7b603f37bf52d566d8a
Stephen Garland <garland@csail.mit.edu>
2020-01-13 09:29:05 -0500

    Script to install xlray.xml for local testing

---
+++ scripts/installXLray.bash
@@ -0,0 +1,3 @@
+# Installing XLray manifest for local use
+/bin/rm -f ~/Library/Containers/com.microsoft.Excel/Data/Documents/wef/*
+cp xlray.xml ~/Library/Containers/com.microsoft.Excel/Data/Documents/wef
