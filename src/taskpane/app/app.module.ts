aad4abf50fce2dce7293b5f06e42dbb3bf397b26
Stephen Garland <garland@csail.mit.edu>
2020-01-14 07:52:39 -0500

    Add declaration of new monitor component

--- src/taskpane/app/app.module.ts
+++ src/taskpane/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import AppComponent from "./app.component";
+import { MonitorComponent } from "./monitor/monitor.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MonitorComponent],
   imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
