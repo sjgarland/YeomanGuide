58c7d1b20c41e3880f02e695947aa42665557f38
Stephen Garland <garland@csail.mit.edu>
2020-01-14 07:50:24 -0500

    Initial attempt at using an Angular component

---
+++ src/taskpane/app/monitor/monitor.component.ts
@@ -0,0 +1,20 @@
+/*------------------------------------------------------------------
+* app/monitor.component.ts
+* Copyright 2019 (c) Stage One Software.  All rights reserved.
+*-----------------------------------------------------------------*/
+
+import { Component } from '@angular/core';
+
+/** 
+ * Controller for the monitor view. 
+ */
+@Component({
+  // eslint-disable-next-line no-undef
+  moduleId: module.id.toString(),
+  selector: 'monitor',
+  templateUrl: './monitor.component.html',
+  styleUrls: ['./monitor.component.css']
+})
+export class MonitorComponent  {
+    title = 'Excel monitor';
+}
