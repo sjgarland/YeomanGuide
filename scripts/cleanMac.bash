a54bfe79ebad8adcd66a7f61f4197987ee4887a6
Stephen Garland <garland@csail.mit.edu>
2020-01-15 15:57:53 -0500

    Clear caches

--- scripts/cleanMac.bash
+++ scripts/cleanMac.bash
@@ -1,2 +1,4 @@
 echo "Removing all manifests"
 rm ~/Library/Containers/com.microsoft.Excel/Data/Documents/wef/*
+echo "Clearing caches"
+/bin/rm -rf ~/Library/Containers/com.microsoft.Excel/Data/Library/Caches/*
