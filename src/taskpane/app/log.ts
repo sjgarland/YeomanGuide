8ea37c80cc95ff2fc30d218ed80d15ade7c622f4
Stephen Garland <garland@csail.mit.edu>
2020-01-20 08:54:03 -0500

    Replace static methods by singleton class

--- src/taskpane/app/log.ts
+++ src/taskpane/app/log.ts
@@ -1,35 +1,47 @@
 /*------------------------------------------------------------------
-* app/log.ts
+* taskpane/app/log.ts
 * Copyright 2020 (c) Stage One Software.
 *-----------------------------------------------------------------*/
 
-/** Static methods that maintain a debugging log. */
+/** Singleton class that maintains a debugging log. */
 export class Log {
+
+    /** Constructs the singleton instance of this class. */
+    private constructor() { }
+
+    /** Returns the singleton instance of this class. */
+    public static getSingleton(): Log { 
+        if (!this.singleton) { this.singleton = new Log(); } 
+        return this.singleton;
+    }
     
-    private static contents: string[] = [];
+    /** The singleton instance of this class. */
+    private static singleton: Log;
+
+    private contents: string[] = [];
     
     /** Flag to display debugging information on the console. */
     private static tracing = false;
     
     /** Adds an entry to the log. */
-    public static add(s: string) {
-        Log.contents.push(Log.getTime() + ' ' + s);
+    public add(s: string) {
+        this.contents.push(this.getTime() + ' ' + s);
         // if (this.tracing) { console.log(s); }
     }
     
     /** Returns a copy of the log. */
-    public static get(): string[] {
-        return [...Log.contents];
+    public get(): string[] {
+        return [...this.contents];
     }
     
     /** Returns the current time in the format hh:mm:ss. */
-    private static getTime(): string {
+    private getTime(): string {
         let d = new Date();
-        return Log.digits(d.getHours()) + ':' + Log.digits(d.getMinutes()) + ':' + Log.digits(d.getSeconds());
+        return this.digits(d.getHours()) + ':' + this.digits(d.getMinutes()) + ':' + this.digits(d.getSeconds());
     }
     
     /** Returns `n` formatted as a string with at least two digits. */
-    private static digits(n: number): string {
+    private digits(n: number): string {
         return n < 10 ? '0' + n : n.toString();
     }
 
