40b19f1e442f0fff941fc6d08c4eb4391d2d5f94
Stephen Garland <garland@csail.mit.edu>
2020-01-20 08:56:14 -0500

    Poll for changes in settings

--- src/taskpane/app/app.component.ts
+++ src/taskpane/app/app.component.ts
@@ -1,12 +1,12 @@
 /*------------------------------------------------------------------
-* app/app.component.ts
+* taskpane/app/app.component.ts
 * Copyright 2020 (c) Stage One Software.
 *-----------------------------------------------------------------*/
 
 import { Component } from "@angular/core";
 import { Log } from "./log";
 const template = require("./app.component.html");
-/* global console, document, Excel, Office, require, window */
+/* global console, document, Excel, localStorage, Office, require, setInterval, window */
 /* eslint-disable no-constant-condition */
 
 @Component({
@@ -16,22 +16,26 @@ const template = require("./app.component.html");
 export default class AppComponent {
   items = ["One", "Two", "Three"];
   selectedRange = "None";
-
+  clickCount = "0";
+  log = Log.getSingleton();
+  
   constructor() {
-    Log.add('Platform: ' + Office.context.platform);
-    Log.add('License: ' + Office.context.license['value']);
-    Log.add('Office version: ' + Office.context.diagnostics.version);
-    Log.add('Browser: ' + window.navigator.appName);
-    Log.add('Browser version: ' + window.navigator.appVersion);
+    this.log.add('Platform: ' + Office.context.platform);
+    this.log.add('License: ' + Office.context.license['value']);
+    this.log.add('Office version: ' + Office.context.diagnostics.version);
+    this.log.add('Browser: ' + window.navigator.appName);
+    this.log.add('Browser version: ' + window.navigator.appVersion);
     let version = 4;
     while (true) {
       if (!Office.context.requirements.isSetSupported('ExcelApi', '1.' + version.toString())) { break; }
       version++;
     }
-    Log.add('ExcelApi version 1.' + version.toString());
+    this.log.add('ExcelApi version 1.' + version.toString());
     this.run();
+    localStorage.setItem('count', '0');
+    this.checkSettings();
   }
-
+  
   async run() {
     try {
       await Excel.run(async context => {
@@ -42,12 +46,37 @@ export default class AppComponent {
       });
     } catch (error) { console.error(error); }
   }
-
-  public log(): string[] { return Log.get() }
+  
+  /**
+  * Polls for changes in `Office.context.documents.settings`.
+  * 
+  * Polling is necessary because Excel fires the context.workbook.settings.onSettingsChanged
+  * event only when the taskpane is displayed and then only when more than one user is
+  * editing/coauthoring the spreadsheet.
+  * 
+  * To minimize interactions with Excel, commands.html mirrors document settings as
+  * items in localStorage.  Using document settings in addition to localStorage items
+  * preserves settings from one session of Excel to the next.
+  * 
+  * Polling may not be needed to detect changes in localStorage.  TODO Try using events
+  * to catch these changes.
+  */
+  private checkSettings() {
+    const context = this; // Needed to give the setInterval function access to `this`
+    setInterval(function() {
+      const newCount = localStorage.getItem('count');
+      if (newCount != null && newCount != context.clickCount) {
+        context.clickCount = newCount;
+        context.log.add("Count button click number " + newCount);
+      }
+    }, 250);
+  }
+  
+  public getClicks(): string { return this.clickCount; }
   
   public logAsTable(): string {
     let s = '<table>';
-    for (const line of Log.get()) { s += '<tr><td>' + line + '</td></tr>'}
+    for (const line of this.log.get()) { s += '<tr><td>' + line + '</td></tr>'}
     s += '</table>';
     document.getElementById('debuggingLog').innerHTML = s;
     return '';
